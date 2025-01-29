6236ba4ea4115e0138c7ae61a2ae4a08e65acaf1
romanmihailow <romanmihailow@gmail.com>
2025-01-29 16:13:23 +0500

    new commit

---
+++ src/Park.java
@@ -0,0 +1,14 @@
+public class Park {
+    public class Attracion{
+        private String name;
+        private String workingHours;
+        private double price;
+
+        public Attracion(String name, String workingHours, double price) {
+            this.name = name;
+            this.workingHours = workingHours;
+            this.price = price;
+        }
+        
+    }
+}
